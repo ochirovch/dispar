542130ec22a317416454c445c17c980647185af6
ochirovch <isenseu@gmail.com>
2021-09-10 00:34:12 +0700

    function deploy - need move to workflows

---
+++ GoogleCloudFunctions/LinkCollector/go.mod
@@ -0,0 +1,3 @@
+module dis.par/linkcollector
+
+go 1.16
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 	"github.com/gorilla/mux"
 )
 
