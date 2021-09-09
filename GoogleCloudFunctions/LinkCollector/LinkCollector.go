1511f00d3f3d3b7ce89cc498726e4acdf7d3c5d3
ochirovch <isenseu@gmail.com>
2021-09-09 22:24:18 +0700

    Link collector function added

--- GoogleCloudFunctions/LinkCollector/LinkCollector.go
+++ GoogleCloudFunctions/LinkCollector/LinkCollector.go
@@ -1,17 +1,31 @@
 package LinkCollector
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gocolly/colly/v2"
+)
+
+const (
+	begin = 1
+	end   = 10
 )
 
 func LinkCollector(w http.ResponseWriter, req *http.Request) {
 	c := colly.NewCollector()
 
 	// add data to pub sub channel page project;url
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c.OnHTML(".news-item__body > a", func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
 	})
-	c.Visit("http://go-colly.org/")
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	for i := begin; i < end; i++ {
+		fmt.Println(i)
+		c.Visit("https://www.chita.ru/news/?pg=" + strconv.Itoa(i))
+	}
 }
--- main.go
+++ main.go
@@ -4,8 +4,15 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 
+	"github.com/gocolly/colly"
 	"github.com/gorilla/mux"
+)
+
+const (
+	begin = 1
+	end   = 100
 )
 
 type Settings struct {
@@ -30,11 +37,32 @@ func robots(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "User-agent: *")
 }
 
+func LinkCollector(w http.ResponseWriter, req *http.Request) {
+
+	c := colly.NewCollector()
+
+	// add data to pub sub channel page project;url
+
+	c.OnHTML("div.news-item__title > a", func(e *colly.HTMLElement) {
+		fmt.Println(e.Request.URL.Host + e.Attr("href"))
+		//		e.Request.Visit(e.Attr("href"))
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	for i := begin; i < end; i++ {
+		fmt.Println(i)
+		c.Visit("https://www.chita.ru/news/?pg=" + strconv.Itoa(i))
+	}
+}
+
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", hello).Methods("GET")
 	router.HandleFunc("/robots.txt", robots).Methods("GET")
+	router.HandleFunc("/linkcollector", LinkCollector).Methods("GET")
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8010", router)
 }
